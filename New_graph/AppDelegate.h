86c04c9069eb571c1a1bdf58ec72fa970db7ac87
Test <test@Kishorekumars-Mac-mini-7.local>
2015-03-18 14:42:27 +0530

    Initial Commit

---
+++ New_graph/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  New_graph
+//
+//  Created by Test on 3/18/15.
+//  Copyright (c) 2015 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ New_graph/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  New_graph
+//
+//  Created by Test on 3/18/15.
+//  Copyright (c) 2015 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
